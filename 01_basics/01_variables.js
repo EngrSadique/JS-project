adc0085a3bd0b46849ce37613c18348179164eb7
EngrSadique <jattsadique@gmail.com>
2024-11-05 02:45:04 +0000

    Container code for project

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+// Variables & Constant    Variables: Jin ko change ker sakty hn , Constant: Jin ko change nhi ker sakty
+// Variables = let & Var            Constant = const
+// Var ko use nhi kerna kyun k is k kuch issues hn realted to (Scope {})
+
+
+
+
+const accountId = 14455
+let accountEmail = "jattsadique@gmail.com"
+var accountPassword = "1234"
+accountCity = "Sargodha"
+let accountTitle;
+
+//  accountId = 77777    // Changes are not Allowed
+ accountEmail = "cc@gamil.com"
+ accountPassword = "4545"
+ accountCity = "Fasalabad"
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountTitle]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // We use it for the latest version of JS
+
+// alert(3 + 3)    // We are using NodeJs, not Browser
+
+console.log( 3 + 3) // Best Practice for writing a code to make it readable
+
+console.log( 3 +    // Not a best practice for writing a code to make it readable
+     3)
+
+
+
+// DATA TYPES 
+// 1: Primitive data type                      // 2: Reference data type 
+        
+// Number => 2 to power 53                     // Object => any thing 
+// String => ""
+// Null   => standalone value
+// Bollean => true/fasle
+// Undefine => 
+// Symbol  => unique
+
+// Practice
+console.log(typeof 18);
+console.log(typeof "siddique")
+console.log(typeof null);
+console.log(typeof Boolean);
+console.log(typeof undefined);
