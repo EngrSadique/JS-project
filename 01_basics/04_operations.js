212fe508a17ed84604251bf7cb2b00eb36364027
EngrSadique <jattsadique@gmail.com>
2024-11-06 02:35:34 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,42 @@
+// Operations
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);    // 2 ki Power k ly
+// console.log(2 % 3);
+
+
+let str1 = "Hello"
+let str2 = " Siddique"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let nam1 , nam2, nam3 
+nam1 = nam2 = nam3 = 2 + 2
+
+// console.log(nam1);
+// console.log(nam2);
+// console.log(nam3);
+
+
+
+// Prefix Postfix Increments
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter);
+
+
+
+
+
