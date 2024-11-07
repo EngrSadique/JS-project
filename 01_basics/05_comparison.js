d37ec8fd4cf637fdb2b20fcc76e48b88676fc606
EngrSadique <jattsadique@gmail.com>
2024-11-07 02:10:33 +0000

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,40 @@
+// Comparison
+
+// console.log(2 == 2);
+
+// console.log("2" == "2");
+
+// console.log(null == null);
+
+// console.log(Boolean == Boolean);
+
+// console.log(undefined == undefined);
+
+// console.log("3" === 3);
+
+// console.log(3 != 4);
+
+// console.log(3 != 3);
+
+
+
+// console.log(null == Boolean);
+
+// console.log(null == undefined);
+
+
+
+
+
+
+// console.log(null > 0);      // False
+// console.log(null == 0);     // False
+// console.log(null < 0);      // False
+// console.log(null >= 0);     // true
+// console.log(null <= 0);     // true
+
+
+
+
+
+
