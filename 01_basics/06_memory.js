3d6ac2d5b67173a24cee06053b4fcdba32ef8d0f
EngrSadique <jattsadique@gmail.com>
2024-11-16 07:37:19 +0000

    Memory.js

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,28 @@
+let nameOne = "Siddique"
+
+let nameTwo = nameOne;
+
+
+nameTwo = "Usman";
+
+// console.log(nameOne);
+// console.log(nameTwo);
+
+let userOne = {
+    email : "Sid@gmail.com",
+    age : 18,
+}
+
+let userTwo = userOne;
+
+userTwo.email = "Usman@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
+
+
