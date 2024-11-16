a6f4aae9a3cdd27b0a631c44ba11713519e2f5ca
EngrSadique <jattsadique@gmail.com>
2024-11-16 07:37:51 +0000

    Dates.js

---
+++ 01_basics/09_dates.js
@@ -0,0 +1,43 @@
+// let myDate = new Date ()
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date (2024,10,16)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date (2024,10,16)
+// console.log(myCreatedDate.toDateString());
+
+
+
+// let myCreatedDate = new Date ("11-16-2024")
+// console.log(myCreatedDate.toString());
+
+// let myDateStamp = Date.now()
+// console.log(myDateStamp);
+// console.log(myDateStamp/1000);
+// console.log(Math.floor(myDateStamp/1000));
+
+
+let myDate = new Date()
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth() + 1);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+// console.log(Math.floor(myDate.getTime()/1000));
+// console.log(myDate.getHours());
+
+// console.log(myDate.toLocaleString("default"));
+
+
+
+
+
+
+
