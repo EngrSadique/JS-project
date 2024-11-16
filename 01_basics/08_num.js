bc6a4c5c9975609c1fbc8ea710be7e7900520093
EngrSadique <jattsadique@gmail.com>
2024-11-16 07:36:57 +0000

    Num.js

---
+++ 01_basics/08_num.js
@@ -0,0 +1,38 @@
+const value = 400
+// console.log(value);
+
+const score = new Number (100000000)
+// console.log(score);
+// console.log(score.toString());
+// console.log(score.toString().length);
+// console.log(score.valueOf());
+// console.log(score.toFixed(2));
+// console.log(score.toPrecision(6));
+// console.log(score.toLocaleString());
+
+
+
+// ++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+
+
+// console.log((Math.floor(Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
+
+
