d742964e190fd3039deb4f735432cd3ad1b79f5b
EngrSadique <jattsadique@gmail.com>
2024-11-16 07:37:35 +0000

    String.js

---
+++ 01_basics/07_string.js
@@ -0,0 +1,28 @@
+let name = "Siddique"
+let age = 22
+
+// console.log(name + age + " Age");
+
+// console.log(`My naame is ${name} and my age is ${age}`);
+
+let gameName = "subway-Sur-face"
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('y'));
+// console.log(gameName.substring( 5));
+// console.log(gameName.slice(4, -8));
+// console.log(gameName.trim());
+// console.log(gameName.split(' w  '));
+let url = "http/sid%20.com"
+console.log(url.replace("%20", "-"));
+
+
+
+
+
+
+
+
