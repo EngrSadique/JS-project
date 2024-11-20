e5e55ac376ea974f449668031a6cd51e4b29a67b
EngrSadique <jattsadique@gmail.com>
2024-11-20 19:38:00 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,48 @@
+// let myArr = [1,2,3,4,5]
+// let myArr2 = ["Sid", "Usman"]
+// let myArr3 = [null]
+// let myArr4 = [true, false]
+// let myArr5 = [undefined]
+
+
+
+// console.log(myArr[0]);
+// console.log(myArr2[0]);
+// console.log(myArr3[0]);
+// console.log(myArr4[0]);
+// console.log(myArr5[0]);
+
+
+
+// Array Methods
+let myArr = new Array (0,1,2,3,4,5)
+
+
+// myArr.push(8)
+// myArr.pop()
+
+// myArr.unshift(8)
+// myArr.shift()
+
+// console.log(myArr);
+
+
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// console.log(myArr);
+// console.log(typeof myArr);
+// console.log(myArr.join());
+// console.log(typeof myArr.join());
+
+console.log(myArr);
+console.log(myArr.slice(0,3));
+console.log(myArr.splice(0,3));
+console.log(myArr);
+
+
+
+
+
