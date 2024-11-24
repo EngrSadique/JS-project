62e59f28b941d6b73d72e12074fd128359bf98bd
EngrSadique <jattsadique@gmail.com>
2024-11-24 19:06:50 +0000

    Array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,25 @@
+const namesOne = ["Sid", "Usman", "Ali"]
+const namesTwo = ["Fazi", "Hamza", "Dani"]
+const namesThree = ["Muneeb", "Aqil"]
+// namesOne.push(namesTwo)
+
+// const totalNames = namesOne.concat(namesTwo)
+
+// const totalNames = [...namesOne, ...namesTwo, ...namesThree]
+
+// console.log(totalNames);
+
+
+// const arr = [1, 2 , 3, [ 4 , 5 ] , 6 , [ 7 , 8 , [ 9 , 0 ] ] ]
+// const realArr = arr.flat(Infinity)
+// console.log(realArr)
+
+
+console.log(Array.isArray("Ali"));
+
+console.log(Array.from("Siddique"));
+console.log(Array.of("Siddique", "Ali", "Usman"));
+
+
+
+
