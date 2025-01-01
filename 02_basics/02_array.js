b9671c5b848d275fbac0505602d3a46ae45c8cde
EngrSadique <jattsadique@gmail.com>
2025-01-01 19:16:13 +0000

    Array2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -15,7 +15,28 @@ const namesThree = ["Muneeb", "Aqil"]
 // console.log(realArr)
 
 
-console.log(Array.isArray("Ali"));
+// console.log(Array.isArray("Ali"));
 
+// console.log(Array.from("Siddique"));
+// console.log(Array.of("Siddique", "Ali", "Usman"));
+
+let arr5 = ["Sadique"]
+let arr6 = ["Ali", "Fizan"]
+
+// arr5.push(9)
+// arr5.pop(9)
+
+// arr5.unshift(9)
+// arr5.shift(9)
+
+// arr5.slice(2)
+
+
+// console.log(arr5.slice(0,3));
+// console.log(arr5.splice(0,3));
+// let arr7 = [...arr5, ...arr6]
+// arr5.flat(1)
 console.log(Array.from("Siddique"));
-console.log(Array.of("Siddique", "Ali", "Usman"));
+
+
+
